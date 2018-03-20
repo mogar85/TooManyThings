05efcba5a53a325f60dbb1a0a2750234b583f352
Martin Klevin <mogar85@hotmail.com>
2018-03-20 16:43:57 +0100

    - Added panel to spawn cubes and spheers from

---
+++ TooManyThings/Assets/scr/ButtonVR.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    [SerializeField]
+    OVRInput.Button inputPress;
+
+    [SerializeField]
+    UnityEvent action;
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("Triggered!");
+        if (OVRInput.Get(inputPress))
+        {
+            action.Invoke();
+        }
+    }
+}
---
+++ TooManyThings/Assets/scr/SpawnObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToSpawn;
+    [SerializeField]
+    Vector3 offSet;
+
+    public void Spawn()
+    {
+        Instantiate(objectToSpawn,transform.position + offSet, transform.rotation);
+    }
+}
